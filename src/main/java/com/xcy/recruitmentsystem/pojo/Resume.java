6c90ee47207c8625d78478a0a1d9c71062f1f016
fsw <1649692109@qq.com>
2019-08-05 22:26:11 +0800

    2019-08-05 22:25

--- src/main/java/com/xcy/recruitmentsystem/pojo/Resume.java
+++ src/main/java/com/xcy/recruitmentsystem/pojo/Resume.java
@@ -39,5 +39,142 @@ public class Resume {
   private String pEduErpe;
 
   private String pStudyExpe;
+
   private String pMyself;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getpName() {
+    return pName;
+  }
+
+  public void setpName(String pName) {
+    this.pName = pName;
+  }
+
+  public String getpBirthday() {
+    return pBirthday;
+  }
+
+  public void setpBirthday(String pBirthday) {
+    this.pBirthday = pBirthday;
+  }
+
+  public String getpEdu() {
+    return pEdu;
+  }
+
+  public void setpEdu(String pEdu) {
+    this.pEdu = pEdu;
+  }
+
+  public String getpGradute() {
+    return pGradute;
+  }
+
+  public void setpGradute(String pGradute) {
+    this.pGradute = pGradute;
+  }
+
+  public String getpSkill() {
+    return pSkill;
+  }
+
+  public void setpSkill(String pSkill) {
+    this.pSkill = pSkill;
+  }
+
+  public String getpPhone() {
+    return pPhone;
+  }
+
+  public void setpPhone(String pPhone) {
+    this.pPhone = pPhone;
+  }
+
+  public String getpWork() {
+    return pWork;
+  }
+
+  public void setpWork(String pWork) {
+    this.pWork = pWork;
+  }
+
+  public String getpEmail() {
+    return pEmail;
+  }
+
+  public void setpEmail(String pEmail) {
+    this.pEmail = pEmail;
+  }
+
+  public String getpAddress() {
+    return pAddress;
+  }
+
+  public void setpAddress(String pAddress) {
+    this.pAddress = pAddress;
+  }
+
+  public String getpWanted() {
+    return pWanted;
+  }
+
+  public void setpWanted(String pWanted) {
+    this.pWanted = pWanted;
+  }
+
+  public String getpLanguage() {
+    return pLanguage;
+  }
+
+  public void setpLanguage(String pLanguage) {
+    this.pLanguage = pLanguage;
+  }
+
+  public String getpOpen() {
+    return pOpen;
+  }
+
+  public void setpOpen(String pOpen) {
+    this.pOpen = pOpen;
+  }
+
+  public String getpExpe() {
+    return pExpe;
+  }
+
+  public void setpExpe(String pExpe) {
+    this.pExpe = pExpe;
+  }
+
+  public String getpEduErpe() {
+    return pEduErpe;
+  }
+
+  public void setpEduErpe(String pEduErpe) {
+    this.pEduErpe = pEduErpe;
+  }
+
+  public String getpStudyExpe() {
+    return pStudyExpe;
+  }
+
+  public void setpStudyExpe(String pStudyExpe) {
+    this.pStudyExpe = pStudyExpe;
+  }
+
+  public String getpMyself() {
+    return pMyself;
+  }
+
+  public void setpMyself(String pMyself) {
+    this.pMyself = pMyself;
+  }
 }
